ff58f2d478a93093b0bcdfcfd439a8550365aec3
CreepyMemes <mjouins@gmail.com>
2024-12-24 09:50:32 +0100

    minor code styling

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,29 @@
-import globals from "globals";
-import eslint from "@eslint/js";
+import eslint from '@eslint/js';
+import globals from 'globals';
 
 import prettier from 'eslint-plugin-prettier/recommended';
-import react from "eslint-plugin-react";
+import react from 'eslint-plugin-react';
 
-
-/** @type {import('eslint').Linter.Config[]} */
 export default [
   {
-    ignores: [
-      "dist/*",
-      "*.js",
-      "*.cjs",
-      "*.mjs",
-    ],
+    ignores: ['dist/*', '*.js', '*.cjs', '*.mjs'],
   },
+
   {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+
     languageOptions: {
+      globals: globals.browser,
+
       parserOptions: {
-        sourceType: "module",
-        ecmaVersion: "latest",
+        sourceType: 'module',
+        ecmaVersion: 'latest',
       },
     },
-  },
-  {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
-    languageOptions: { globals: globals.browser },
+
     settings: {
       react: {
-        version: "detect",
+        version: 'detect',
       },
     },
   },
